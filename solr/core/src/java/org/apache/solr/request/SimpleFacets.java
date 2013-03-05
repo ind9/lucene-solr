d2f214ac929a3ad061755c0cb02416a1e6841b78
J Mohamed Zahoor <zahoor@indix.com>
2013-03-05 16:55:26 +0530

    - Removed some sout's

--- solr/core/src/java/org/apache/solr/request/SimpleFacets.java
+++ solr/core/src/java/org/apache/solr/request/SimpleFacets.java
@@ -441,7 +441,6 @@ public class SimpleFacets {
 
 
       for (String f : facetFs) {
-        System.out.println("******* Resolved facet field = " + f);
         parseParams(FacetParams.FACET_FIELD, f);
         String termList = localParams == null ? null : localParams.get(CommonParams.TERMS);
         if (termList != null) {
