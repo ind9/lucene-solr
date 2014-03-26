fe72976a3f200137ef043afee04a167bf9761d7b
S Sesha Sendhil <seshasendhil@gmail.com>
2014-03-26 17:10:03 +0530

    add missing import

--- solr/core/src/java/org/apache/solr/handler/component/FacetComponent.java
+++ solr/core/src/java/org/apache/solr/handler/component/FacetComponent.java
@@ -26,6 +26,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Collection;
 
 import org.apache.lucene.util.FixedBitSet;
 import org.apache.solr.common.SolrException;
